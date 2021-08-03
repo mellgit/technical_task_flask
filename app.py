8c4713300742afca16658627b9e0bfeb346c5082
mellgit <daniilhorn8@gmail.com>
2021-08-03 15:26:02 +0300

    creating a json file and writing to json file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+import json
 from flask import Flask, request, abort
 from requests import get 
 from json import dumps # представить словарь в виде json 
@@ -15,11 +16,46 @@ app = Flask(__name__)
 # декоратор обертка ф-и
 @app.route("/")
 def index():
+
+
     # Анализируемые параметры URL-адреса (часть URL-адреса после вопросительного знака).
-    email = request.args.get('email')
-    if not email:
-        # Flask return 404 - google
-        return abort(404)
+    # email = request.args.get('eehzntm5@hotmail.com')
+    # if not email:
+    #     # Flask return 404 - google
+    #     return abort(404)
+
+
+    email = "eehzntm5@hotmail.com"
+    # получить md5 hash
+    import hashlib
+    hashed_email = hashlib.md5(email.encode('utf-8')).hexdigest()
+
+    # получение json объекта
+    response = get(f"https://ru.gravatar.com/{hashed_email}.json")
+
+    if response.status_code == 200:
+        need_to_edit = response.json()
+        john: dict = need_to_edit['entry'][0]
+        new_john = {}
+        new_john["id"] = john.get('id', None)
+        new_john["email_hash"] = john.get('hash', None)
+        new_john["url"] = john.get('profileUrl', None)
+        new_john["alias"] = john.get('preferredUsername', None)
+        new_john["thumb"] = john.get('thumbnailUrl', None)
+        new_john["photos"] = john.get('photos', None)
+        new_john["person"] = john.get('name.formatted', None)
+        new_john["location"] = john.get('currentLocation', None)
+        new_john["emails"] = john.get('emails', None)
+        new_john["accounts"] = john.get('accounts', None)
+        new_john["urls"] = john.get('urls', None)
+
+        with open("examle.json", "w") as json_file:
+            json.dump(new_john, json_file, indent=4)
+        
+        from pprint import pprint
+        pprint(new_john)
+        return new_john
+
     
 
     return abort(400)
